0482c53a17936e19e9c2da74498bfe44fc080240
zerodice0 <zerodice0@gmail.com>
2024-06-09 01:35:33 +0900

    fix: add copy keys. - change layers name. - add screen capture keys to layer 4.

--- keymap.c
+++ keymap.c
@@ -4,25 +4,31 @@
 #include <stdint.h>
 #include QMK_KEYBOARD_H
 
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 3
-#define _ADJUST2 4
+#define _BASIC 0
+#define _LEFT_UNDER 1
+#define _RIGHT_UNDER 2
+#define _BOTH_UNDER 3
+#define _LEFT_UP 4
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  ADJUST,
-  ADJUST2,
+  BASIC = SAFE_RANGE,
+  LEFT_UNDER,
+  RIGHT_UNDER,
+  BOTH_UNDER,
+  LEFT_UP,
 };
 
 const uint16_t KC_EMOJI = LGUI(LCTL(KC_SPACE));
 const uint16_t KC_AG = OSM(MOD_LALT|MOD_LGUI);
+const uint16_t KC_SG3 = LSG(KC_3);
+const uint16_t KC_SGC3 = LSG(LCTL(KC_3));
+const uint16_t KC_SG4 = LSG(KC_4);
+const uint16_t KC_SGC4 = LSG(LCTL(KC_4));
+const uint16_t KC_SG5 = LSG(KC_5);
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT(
+  [_BASIC] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -32,11 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MO(4),            KC_AG,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI, LOWER,   KC_ENT,                    KC_SPC,  RAISE,   KC_RALT
+                                    KC_LGUI, LEFT_UNDER,KC_ENT,              KC_SPC,   RIGHT_UNDER, KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_LOWER] = LAYOUT(
+  [_LEFT_UNDER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -50,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_RAISE] = LAYOUT(
+  [_RIGHT_UNDER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -64,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_ADJUST] = LAYOUT(
+  [_BOTH_UNDER] = LAYOUT(
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, _______, _______, _______, _______,                            KC_MCTL, KC_LPAD, _______, _______, _______, QK_BOOT,
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
@@ -78,9 +84,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_ADJUST2] = LAYOUT(
+  [_LEFT_UP] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     RGB_M_P, _______, _______, _______, _______, RGB_MOD,                            _______, _______, _______, KC_ACL0, KC_ACL1, KC_ACL2,
+     RGB_M_P, _______, _______, _______, _______, RGB_MOD,                            KC_SG4,  KC_SGC4, KC_SG3,  KC_SGC3, KC_SG5, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      RGB_M_B, _______, _______, _______, _______, BL_STEP,                            KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -90,43 +96,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
-  )
+  ),
 
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case QWERTY:
+    case BASIC:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
+        set_single_persistent_default_layer(_BASIC);
       }
       return false;
       break;
-    case LOWER:
+    case LEFT_UNDER:
       if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        layer_on(_LEFT_UNDER);
+        update_tri_layer(_LEFT_UNDER, _RIGHT_UNDER, _BOTH_UNDER);
       } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        layer_off(_LEFT_UNDER);
+        update_tri_layer(_LEFT_UNDER, _RIGHT_UNDER, _BOTH_UNDER);
       }
       return false;
       break;
-    case RAISE:
+    case RIGHT_UNDER:
       if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        layer_on(_RIGHT_UNDER);
+        update_tri_layer(_LEFT_UNDER, _RIGHT_UNDER, _BOTH_UNDER);
       } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        layer_off(_RIGHT_UNDER);
+        update_tri_layer(_LEFT_UNDER, _RIGHT_UNDER, _BOTH_UNDER);
       }
       return false;
       break;
-    case ADJUST:
+    case BOTH_UNDER:
       if (record->event.pressed) {
-        layer_on(_ADJUST);
+        layer_on(_BOTH_UNDER);
       } else {
-        layer_off(_ADJUST);
+        layer_off(_BOTH_UNDER);
       }
       return false;
       break;
